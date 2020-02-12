c683641c1452dcdc68b16a757bf81dfad9a3af57
AlbertMartorell <albert.martorell@vicensvivesdigital.com>
2020-02-12 12:48:31 +0100

    Organize build.gradle (I)

--- app/src/test/java/com/apps/albertmartorell/meteomarto/ExampleUnitTest.kt
+++ app/src/test/java/com/apps/albertmartorell/meteomarto/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.apps.albertmartorell.meteomarto
 
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,36 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    // abans de cada test d'aquesta suit s'executa el setup. Així aïllem aquest test dels altres
+    @Before
+    fun setup() {
+
+
     }
+
+    // després de cada test d'aquesta suit s'executa el tearDown. Així aïllem aquest test dels altres
+    @After
+    fun tearDown() {
+
+
+    }
+
+    @Test
+    fun hol() {
+
+        // només necessari per a corrutines
+        runBlocking {
+
+        }
+        // GIVEN: la part de configuraió del test. l?estat que el volem posar abans de començar
+
+        // WHEN: el codi a execugtar
+
+        // THEN: comprovem si s'han generat els resultats esperats a WHEN. Sinó, el test ha fallat
+
+        //assertEquals(4, 2 + 2)
+
+    }
+
 }
