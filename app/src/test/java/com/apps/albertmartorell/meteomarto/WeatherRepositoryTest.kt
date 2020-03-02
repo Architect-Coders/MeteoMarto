ce640ba630e7b4c4c5da7dc795bd38b305dc8f0f
AlbertMartorell <albert.martorell@vicensvivesdigital.com>
2020-03-02 22:42:46 +0100

    JUnit Test for Weather repository (II)

--- app/src/test/java/com/apps/albertmartorell/meteomarto/WeatherRepositoryTest.kt
+++ app/src/test/java/com/apps/albertmartorell/meteomarto/WeatherRepositoryTest.kt
@@ -2,8 +2,9 @@ package com.apps.albertmartorell.meteomarto
 
 import albertmartorell.com.data.repositories.WeatherRepository
 import albertmartorell.com.domain.responses.City
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
-import junit.framework.Assert.assertEquals
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.runBlocking
 import org.junit.After
@@ -41,17 +42,17 @@ class WeatherRepositoryTest {
 
         runBlocking {
 
-            // **** Given
+            // **** Given: la inicialització del test. L'estat en el que volem que començo
             val localCoordinates = mockCoordinates.copy(10F, 40F)
             val localCity = mockFlowCity(localCoordinates, "Sabadell")
             val givenCity: Flow<City> = localCity
 
-            // **** When
+            // **** When: l'execució del codi
             whenever(deviceSource.isEmpty()).thenReturn(false)
             whenever(deviceSource.getCity()).thenReturn(givenCity)
             val savedCity = weatherRepository.getCityWeatherFromDatabase()
 
-            // **** Then
+            // **** Then: verificar que el When s'ha complert
             assertEquals(localCity, savedCity)
 
         }
@@ -63,11 +64,13 @@ class WeatherRepositoryTest {
 
         runBlocking {
 
-            // **** Given
+            // **** Given: la inicialització del test. L'estat en el que volem que començo
             val currentCoordinates = mockCoordinates.copy(50F, 4F)
             val givenCity = mockDomainCity(currentCoordinates, "Eibar")
 
-            // **** When
+            // Configurem els mocks: quan executem una funció retorni un cert valor. En
+            // aquest cas quan la funció getWeatherByCoordinates() és cridada amb els valors
+            // currentCoordinates, retorni l'objecte givenCity
             whenever(
                 serverSource.getWeatherByCoordinates(
                     currentCoordinates.latitude,
@@ -75,16 +78,49 @@ class WeatherRepositoryTest {
                 )
             ).thenReturn(givenCity)
 
-            whenever(
-                serverSource.getWeatherByCoordinates(
-                    currentCoordinates.latitude, currentCoordinates.longitude
-                )
-            ).thenReturn(givenCity)
-            // **** Then
+            // **** When: l'execució del codi
+            val serverCity = serverSource.getWeatherByCoordinates(
+                currentCoordinates.latitude,
+                currentCoordinates.longitude
+            )
 
-            todo
+            // **** Then: verificar que el When s'ha complert
+            assertEquals(givenCity, serverCity)
 
         }
+
+    }
+
+    @Test
+    fun `verifies data got from server saved on local`() {
+
+        runBlocking {
+
+            val currentCoordinates = mockCoordinates.copy(59F, 6F)
+            val givenCity = mockDomainCity(currentCoordinates, "Sant Esteve d'en Bas")
+
+            //whenever(
+            //    serverSource.getWeatherByCoordinates(
+            //        currentCoordinates.latitude,
+            //        currentCoordinates.longitude
+            //    )
+            //).thenReturn(givenCity)
+
+            //weatherRepository.requestWeatherByCoordinates(
+            //    currentCoordinates.latitude,
+            //    currentCoordinates.longitude
+            //)
+
+            deviceSource.saveCityWeather(givenCity)
+
+            //verify(serverSource).getWeatherByCoordinates(
+            //    currentCoordinates.latitude,
+            //    currentCoordinates.longitude
+            //)
+            verify(deviceSource).saveCityWeather(givenCity)
+
+        }
+
     }
 
 }
