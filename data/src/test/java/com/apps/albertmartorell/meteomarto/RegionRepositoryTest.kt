a3dc8c31f5bfdd5abfaa3fb2b4a03dfae75dd0b4
Albert Martorell <albert.martorell@vicensvivesdigital.com>
2020-10-08 11:04:20 +0200

    Moved repository tests to data module (II)

--- data/src/test/java/com/apps/albertmartorell/meteomarto/RegionRepositoryTest.kt
+++ data/src/test/java/com/apps/albertmartorell/meteomarto/RegionRepositoryTest.kt
@@ -1,4 +1,4 @@
-package tests.com.apps.albertmartorell.meteomarto
+package com.apps.albertmartorell
 
 import albertmartorell.com.data.repositories.PermissionChecker
 import albertmartorell.com.data.repositories.RegionRepository
